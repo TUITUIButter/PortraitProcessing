f454ec3fc24e88a493af2a28a2429b1cf1d8bc9f
Witness521 <2320815132@qq.com>
2023-05-14 23:20:12 +0800

    main.py中添加上AI打分

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ from modules.saturation import Saturation
 from modules.utils import Utils
 import cv2
 
-# image = cv2.imread('./imgs/test.png')  # angle: 5.57°
-image = cv2.imread("./imgs/middle-tilt.jpg")  # 倾斜的图片 angle: 25.91°
-# image = cv2.imread("./imgs/02dark.png")  # angle: 5.38°
-# image = cv2.imread("./imgs/cloudy.jpg")  # angle: 5.38°
-# image = cv2.imread("./imgs/good-001.JPG")  # 找不到LHip这个点
-# image = cv2.imread("./imgs/good-002.JPG")  # 背影无法定位
-# image = cv2.imread("./imgs/good-010.JPEG")  # angle: 10.78°
-# image = cv2.imread("imgs/bad-001.JPG")  # angle: 3.59°
+path = "./imgs/middle-tilt.jpg"  # 倾斜的图片 angle: 25.91°
+# path = './imgs/test.png'  # angle: 5.57°
+# path = "./imgs/02dark.png"  # angle: 5.38°
+# path = "./imgs/cloudy.jpg"  # angle: 5.38°
+# path = "./imgs/good-001.JPG"  # 找不到LHip这个点
+# path = "./imgs/good-002.JPG"  # 背影无法定位
+# path = "./imgs/good-010.JPEG"  # angle: 10.78°
+# path = "imgs/bad-001.JPG"  # angle: 3.59°
+
+image = cv2.imread(path)  # 倾斜的图片 angle: 25.91°
 assert image is not None
 
 # 布局
@@ -25,7 +27,7 @@ adjusted = brightness.opt_img(image)  # 调整亮度
 
 # 饱和度
 saturation = Saturation()
-saturation_score = saturation.cal_score(adjusted)  # 饱和度得分
+saturation_score = saturation.cal_score(image)  # 饱和度得分
 adjusted = saturation.opt_img(adjusted)  # 调整饱和度
 
 # TODO 添加抠图与虚化，虚化效果小一点
@@ -34,10 +36,13 @@ alphargb, human, background, human_blur_background = Utils.separate_character(im
 adjusted = human_blur_background
 
 total_score = layout_score + brightness_score + saturation_score
+
 # TODO 加上AI打分
+ai_score = float(Utils.eval_pic(path)[0][0])  # 获取第一个值并转成float类型
 
 
-print(total_score)
+print('total_score', total_score)
+print('AI Score', ai_score)
 
 cv2.namedWindow('Original', cv2.WINDOW_KEEPRATIO)    # 窗口大小可以改变
 cv2.imshow("Original", image)

69d5957774d4890d7c0e13fcd1f934e7927bc41c
Witness521 <2320815132@qq.com>
2023-06-06 23:04:35 +0800

    调整前后打分对比

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ import cv2
 import time
 
 # path = "./imgs/middle-tilt.jpg"  # 倾斜的图片 angle: 25.91°
-# path = './imgs/test.png'  # angle: 5.57°
+path = './imgs/test.png'  # angle: 5.57°
 # path = "./imgs/02.jpg"  # angle: 5.38°
 # path = "./imgs/cloudy.jpg"  # angle: 5.38°
 # path = "./imgs/good-001.JPG"  # 找不到LHip这个点
 # path = "./imgs/good-002.JPG"  # 背影无法定位
 # path = "./imgs/good-010.JPEG"  # angle: 10.78°
 # path = "imgs/bad-001.JPG"  # angle: 3.59°
-path = "imgs/09.jpg"
+# path = "imgs/09.jpg"
 
 image = cv2.imread(path)  # 倾斜的图片 angle: 25.91°
 assert image is not None
@@ -31,19 +31,24 @@ portraitSeg = PortraitSeg()
 
 
 begin = time.perf_counter()
+# 对原始图片进行打分
 layout_score = layout.cal_score(image)  # 布局得分
 brightness_score = brightness.cal_score(image)  # 亮度得分
 saturation_score = saturation.cal_score(image)  # 饱和度得分
-# ai_score = float(NIMA_predict.eval_pic_by_NIMA(path)[0][0])  # AI打分，获取第一个值并转成float类型
-ai_score = NIMA_predict.eval_pic_by_NIMA(path)
-total_score = layout_score + brightness_score + saturation_score
+ai_score = float(NIMA_predict.eval_pic_by_NIMA(path)[0][0])  # AI打分，获取第一个值并转成float类型
+# ai_score = NIMA_predict.eval_pic_by_NIMA(path)
+print('AI Score', ai_score)
+total_score = layout_score + brightness_score + saturation_score + ai_score
 end = time.perf_counter()
 print('cal_score time: {}s'.format(end-begin))
 
 print('total_score', total_score)
-print('AI Score', ai_score)
+print('Average score', total_score / 4)
+# #################################
 
 begin = time.perf_counter()
+# 对图像进行调整
+
 # 亮度
 adjusted = brightness.opt_img(image)  # 调整亮度
 adjusted = saturation.opt_img(adjusted)  # 调整饱和度
@@ -55,6 +60,14 @@ adjusted = human_blur_background
 denoising.run(adjusted)  # 最后一步去噪，回保存到res.jpg
 end = time.perf_counter()
 print('adjust time: {}s'.format(end-begin))
+
+# 对调整后的图像进行打分
+layout_score = layout.cal_score(image)  # 布局得分
+brightness_score = brightness.cal_score(image)  # 亮度得分
+saturation_score = saturation.cal_score(image)  # 饱和度得分
+ai_score = float(NIMA_predict.eval_pic_by_NIMA(path)[0][0])  # AI打分，获取第一个值并转成float类型
+total_score = layout_score + brightness_score + saturation_score + ai_score
+print('调整后的Average score', total_score / 4)
 
 # cv2.namedWindow('Original', cv2.WINDOW_KEEPRATIO)    # 窗口大小可以改变
 # cv2.imshow("Original", image)
